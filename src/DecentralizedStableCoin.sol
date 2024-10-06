345a5d09d0fc0e88f618763d8c8699ab2031d122
Gere_X <52193900+Gere321123@users.noreply.github.com>
2024-10-06 16:26:31 +0200

    add

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.1.18;
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Gergely Gere
+ * Collateral: Exogenous (ETH & BIC)
+ * Minting: Algorithmic
+ * Relative Stablily: Regged to USD
+ */
+
+contract DecentralizedStableCoin {
+
+}
